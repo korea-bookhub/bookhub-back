c046220afcdd892b50efd2b7dc56154ad968c1b6
kohyeokjae <kohj0686@naver.com>
2025-05-19 10:24:11 +0900

    Spring Boot 프로젝트 생성 및 의존성 생성

---
+++ src/test/java/com/bookhub/bookhub_back/BookhubBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookhub.bookhub_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhubBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
