05cf0c44b3b5d32bda703c96fd3a353d9b236151
seoyoonchoi <chsu0904@naver.com>
2025-05-30 13:50:30 +0900

    feat: statisticsFiles - csy

--- src/main/java/com/bookhub/bookhub_back/service/impl/StatisticsStockServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.bookhub.bookhub_back.service.impl;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class StatisticsStockServiceImpl {
-}
