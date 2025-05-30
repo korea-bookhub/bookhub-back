3895e3820f58746fd2883ead434d15650b188a31
Bottomdeal <zpalqm1357@nate.com>
2025-05-30 14:36:49 +0900

    feat: sjw UploadFile 엔티티, 리포지토리 생성

---
+++ src/main/java/com/bookhub/bookhub_back/repository/UploadFileRepository.java
@@ -0,0 +1,9 @@
+package com.bookhub.bookhub_back.repository;
+
+import com.bookhub.bookhub_back.entity.UploadFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UploadFileRepository extends JpaRepository<UploadFile, Long> {
+}
