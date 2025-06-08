adc11bddf8d0a6ea6613a514b5bd4186e8b8a9e7
seoyoonchoi <chsu0904@naver.com>
2025-06-08 15:27:31 +0900

    refactor:csy stock-log

--- src/main/java/com/bookhub/bookhub_back/controller/StockLogController.java
+++ src/main/java/com/bookhub/bookhub_back/controller/StockLogController.java
@@ -1,21 +1,27 @@
 package com.bookhub.bookhub_back.controller;
 
+import com.bookhub.bookhub_back.common.constants.ApiMappingPattern;
+import com.bookhub.bookhub_back.dto.ResponseDto;
+import com.bookhub.bookhub_back.dto.stock.response.StockListResponseDto;
+import com.bookhub.bookhub_back.dto.stock.response.StockUpdateResponseDto;
+import com.bookhub.bookhub_back.service.StockLogService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping(ApiMappingPattern.BASIC_API + ApiMappingPattern.ADMIN_API + "/stock-logs")
 @RequiredArgsConstructor
 public class StockLogController {
-//@PreAuthorize("hasRole('ADMIN')")
-//    @PutMapping(PUT_AUTHORITY_DEMOTE)
+    private final StockLogService stockLogService;
 
-
-    
     //branch 의 로그 전체 조회
+
+
 
     //branch + 유형별 로그 전체조회
 
---
+++ src/main/java/com/bookhub/bookhub_back/service/impl/StockLogServiceImpl.java
@@ -0,0 +1,8 @@
+package com.bookhub.bookhub_back.service.impl;
+
+import com.bookhub.bookhub_back.service.StockLogService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockLogServiceImpl implements StockLogService {
+}
