a04f70a036a62a1f2d622f6761a755a0eafd0b74
kohyeokjae <kohj0686@naver.com>
2025-06-10 22:30:42 +0900

    feat: Emplyee khj

--- src/main/java/com/bookhub/bookhub_back/dto/employee/request/EmployeeOrganizationUpdateRequestDto.java
+++ src/main/java/com/bookhub/bookhub_back/dto/employee/request/EmployeeOrganizationUpdateRequestDto.java
@@ -1,4 +1,14 @@
 package com.bookhub.bookhub_back.dto.employee.request;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class EmployeeOrganizationUpdateRequestDto {
+    private Long branchId;
+    private Long positionId;
+    private Long authorityId;
 }
